5219760d5b7fc6f6eaceb92adcde6f7bb3af2714
Eric VIALLE <ericvialle_axabanque>
2017-07-20 23:55:37 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CloudwatchsElasticBeanStalkDemo
+
 A demo showing how is configured the Cloudwatch Agent for a Docker deployed with AWS Elastic BeanStalk
+
+This demo is based on Spring Boot.
---
+++ src/main/java/org/vialle/demoapp/cloudwatchlogs/Application.java
@@ -0,0 +1,15 @@
+package org.vialle.demoapp.cloudwatchlogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Launcher.
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(Application.class, args);
+  }
+}
